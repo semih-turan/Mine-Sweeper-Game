a1f39bf7275b20b7388d0cea056b74156ab82029
Semih Turan <semituran@gmail.com>
2024-03-03 22:20:28 +0300

    add prompting play area and validations

--- MineSweeperGame/src/Main.java
+++ MineSweeperGame/src/Main.java
@@ -1,9 +1,42 @@
+import java.util.Scanner;
+
 /**
  * @author Semih Turan
  * @since march 2024
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        // Declare variables
+        Scanner input = new Scanner(System.in);
+        int width, height;
+
+        // Prompting play area width and height
+        do{
+            System.out.print("Please enter play area width (min. 2): ");
+            while(!input.hasNextInt()){
+                System.out.println("Please enter a valid number for width!");
+                System.out.print("Please enter play area width (min. 2): ");
+                input.next();
+            }
+            width = input.nextInt();
+            if(width < 2){
+                System.out.println("Width must be at least 2!");
+            }
+        }while(width<2);
+        do{
+            System.out.print("Please enter play area height (min. 2): ");
+            while(!input.hasNextInt()){
+                System.out.println("Please enter a valid number for height!");
+                System.out.print("Please enter play area height (min. 2): ");
+                input.next();
+            }
+            height = input.nextInt();
+            if(height < 2){
+                System.out.println("Height must be at least 2!");
+            }
+        }while(height < 2);
+
+        // Run game
+
     }
 }
