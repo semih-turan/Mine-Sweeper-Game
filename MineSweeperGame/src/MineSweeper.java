8c4b9435fe66fd9c0531a02cdad723867ea9d49e
Semih Turan <semituran@gmail.com>
2024-03-04 10:16:34 +0300

    add run method with user input and checks win/loose conditions

--- MineSweeperGame/src/MineSweeper.java
+++ MineSweeperGame/src/MineSweeper.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 public class MineSweeper {
    // Declare variables
@@ -21,28 +22,114 @@ public class MineSweeper {
         print2DArray(plantMines(this.hidePlayMap));
         System.out.println("--------------------");
         // Printing play map without bombs
-        print2DArray(fill(this.visiblePlayMap,"-"));
+        print2DArray(fill(this.visiblePlayMap));
+
+        // Declare variables
+        int openedCells = 0;
+        int countRow = -1;
+        int countCol = -1;
+        boolean gameOver = false;
+
+        while (!gameOver) {
+            // Prompting user input for row
+            System.out.print("Please select row: ");
+            Scanner input = new Scanner(System.in);
+            int selectRow = input.nextInt();
+
+            // User input validation
+            if(selectRow<0 || selectRow>this.width){
+                System.out.println("Please choose correct row within play area!");
+                continue;
+            }
+
+            // Prompting user input for column
+            System.out.print("Please select column: ");
+            int selectCol = input.nextInt();
+
+            // User input validation
+            if(selectCol < 0 || selectCol>this.height){
+                System.out.println("Please choose correct col within play area!");
+                continue;
+            }
+
+            // Verification to prevent same input
+            if(countRow == selectRow && countCol == selectCol){
+                System.out.println("Please do not enter same values!");
+                continue;
+            }
+
+            // Stores each user input for prevent enter same values
+            countRow = selectRow;
+            countCol = selectCol;
+
+            if(!visiblePlayMap[selectRow][selectCol].equals("-")){
+                System.out.println("You enter this value before!");
+                continue;
+            }
+
+            // Game over condition or printing number of mines around selected point
+            if (hidePlayMap[selectRow][selectCol].equals("*")) {
+                System.out.println("Boom!!!  You are failed!");
+                gameOver = true;
+            } else {
+                // Sets boundary conditions for playing area
+                int conditionOne = Math.max(0, selectRow - 1);
+                int conditionTwo = Math.min(this.width - 1, selectRow + 1);
+                int conditionThree = Math.max(0, selectCol - 1);
+                int conditionFour = Math.min(this.height - 1, selectCol + 1);
+
+                // Calculate the number of mines in the play area
+                int mineCount = 0;
+                for (int i = conditionOne; i <= conditionTwo; i++) {
+                    for (int j = conditionThree; j <= conditionFour; j++) {
+                        if (hidePlayMap[i][j].equals("*")) {
+                            mineCount++;
+                        }
+                    }
+                }
+
+                // Counts all opened cells
+                for (int i = 0; i < width; i++) {
+                    for (int j = 0; j < height; j++) {
+                        if (!visiblePlayMap[i][j].equals("-")) {
+                            openedCells++;
+                        }
+                    }
+                }
+                // Checks win conditions
+                if (openedCells == ((width * height) - this.numberOfMines)-1) { // Minus 1 due to printing message before loop
+                    System.out.println("Congratulations! You have won the game!");
+                    gameOver = true;
+                } else{
+                    openedCells = 0;
+                }
+
+                // Printing visible map
+                visiblePlayMap[selectRow][selectCol] = Integer.toString(mineCount);
+                print2DArray(visiblePlayMap);
+            }
+        }
     }
 
     // Planting mines method
     private String[][] plantMines(String[][] arr){
         // Type casting
-        double width = (double) this.width;
-        double height = (double) this.height;
+        double width = this.width;
+        double height = this.height;
 
         // Calculating number of mines in the play area
         this.numberOfMines = (int) Math.floor((width * height)/4);
 
         // Fill mine map with "-"
-        fill(arr,"-");
+        fill(arr);
 
         // Insert "*" bombs randomly
         int bombCount =  numberOfMines;
         while(!(bombCount==0)) {
-            int randomRow = (int) (Math.random() * this.width); // The Evolution Form: Item 8
-            int randomCol = (int) (Math.random() * this.height); // The Evolution Form: Item 8
+            int randomRow = (int) (Math.random() * this.width);
+            int randomCol = (int) (Math.random() * this.height);
             if (!(arr[randomRow][randomCol].equals("*"))) {
-                arr[randomRow][randomCol] = "*";
+                arr[randomRow][randomCol] = "*";  // The Evaluation Form: Item 8
                 bombCount--;
             }
         }
@@ -59,13 +146,11 @@ public class MineSweeper {
         }
     }
 
-    // Fill array method
-    static String[][] fill(String[][] arr, String value){
+    // Fill array "-" method
+    static String[][] fill(String[][] arr){
 
-        for(int indexRow = 0; indexRow < arr.length; indexRow++){
-            for(int indexCol = 0; indexCol < arr[indexRow].length; indexCol++){
-                arr[indexRow][indexCol] = value;
-            }
+        for (String[] strings : arr) {
+            Arrays.fill(strings, "-");
         }
         return arr;
     }
